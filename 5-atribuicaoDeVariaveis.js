7eceabe39658c1e0b7b9ea0e33eb9af0c8ba8985
Helio Monteiro <heliojr.monteiro@yahoo.com.br>
2024-01-30 18:21:17 -0300

    const e let, case sensitive, comentários, parseInt e parseFloat, casa decimal e atribuicao de variaveis

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Atribuição de Variáveis");
+const idade = 29;
+const primeiroNome = "Hélio";
+const sobrenome = "Monteiro";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome,sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+//nomeCompleto = 2;
